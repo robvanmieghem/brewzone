fd66d2639bf93837fc0decfb433a1e6af5d54a97
RobVanMieghem <rob@awingu.com>
2013-06-20 10:25:53 +0200

    Cleaned it up a bit

--- mashcontrol/static/js/app.js
+++ mashcontrol/static/js/app.js
@@ -2,7 +2,7 @@ angular.module('brewzone', ['$strap.directives']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
   	  when('/', {templateUrl: 'partials/home.html'}).
-      when('/state', {templateUrl: 'partials/state.html',   controller: BrewzoneController}).
+      when('/state', {templateUrl: 'partials/state.html'}).
       when('/history', {templateUrl: 'partials/history.html', controller: BrewzoneController}).
       when('/settings', {templateUrl: 'partials/settings.html', controller: BrewzoneController}).
       otherwise({redirectTo: '/'});
@@ -12,7 +12,7 @@ angular.module('brewzone', ['$strap.directives']).
         link: function (scope, elem, attrs) {
         		var updateChart = function(data){
         			elem.empty();
-    				$.jqplot(elem.attr('id'),[ data ]);
+    				$.jqplot(attrs.id,[ data ]);
         		}
         
         		scope.$watch(attrs.ngModel, function(newValue, oldValue) {
